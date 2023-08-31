380b6bede2e4669e4675e75d79bbd4eaec5f7406
EstebanVC <estebanvasquezc20010106@gmail.com>
2023-08-31 15:57:19 -0500

    Cambios videollamada

--- EDO.py
+++ EDO.py
@@ -8,9 +8,9 @@ Created on Tue Aug 29 16:32:39 2023
 = Esteban Vásquez Cano
 """
 
-def euler_method(f, x0=0, y0=0, tamaño_paso=0.1, numero_pasos=5):
+def euler_method(f, x0=0, y0=0, tamaño_paso=0.1, numero_pasos=10):
 # =============================================================================
-#     esta funcion recibe los datos iniciales de x & y, tambien el tamaño de paso y número de pasos,
+#     Esta función recibe los datos iniciales de x & y, también el tamaño de paso y número de pasos,
 #     también recibe la ecuacion evaluada con eval() para luego crear y llenar la tabla con un ciclo for y 
 #     utilizando la operacion matematica vista en el video
 # =============================================================================
@@ -21,7 +21,7 @@ def euler_method(f, x0=0, y0=0, tamaño_paso=0.1, numero_pasos=5):
     print(' ITERACIÓN \t|    X\t |\t   Y')
     print('='*40)
    
-    for paso in range(numero_pasos+1):
+    for paso in range(numero_pasos + 1):
         x_new= x
         y_new = y
         
@@ -40,10 +40,10 @@ def evaluar_ecuacion(x, y):
     
 print("\nEste script soluciona una ecuación diferencial ordinaria de primer orden por el método de Euler.\n")
 # Ingresar las condiciones iniciales, tamaño del paso y número de pasos
-x0 = float(input("Ingrese el valor de la condición inicial de x: "))
-y0 = float(input("Ingrese el valor de la condición inicial de y: "))
-tamaño_paso = float(input("Ingrese el tamaño del paso: "))
-numero_pasos = int(input("Ingrese el número de pasos: "))
+# x0 = float(input("Ingrese el valor de la condición inicial de x: "))
+# y0 = float(input("Ingrese el valor de la condición inicial de y: "))
+# tamaño_paso = float(input("Ingrese el tamaño del paso: "))
+# numero_pasos = int(input("Ingrese el número de pasos: "))
 # =============================================================================
 # Entramos a un ciclo While() para pedir al usuario la ecuación y procedemos a evaluarla, utilizando un if-else
 # mientras no se cumpla la condicion seguira pidiendo la ecuación en terminos de x & y, asegurando las 
@@ -58,4 +58,4 @@ while True:
 resultado = ecuacion
 
 # Resolver la EDO usando el método de Euler
-euler_method(evaluar_ecuacion, x0, y0, tamaño_paso, numero_pasos)
+euler_method(evaluar_ecuacion)
